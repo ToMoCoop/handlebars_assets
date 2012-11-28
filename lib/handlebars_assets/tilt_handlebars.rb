d120203f641616e5eba83436473188463736875c
Fred Thompson <fred.thompson@buildempire.co.uk>
2012-11-28 06:20:55 +0000

    This regex is driving me crazy!

--- lib/handlebars_assets/tilt_handlebars.rb
+++ lib/handlebars_assets/tilt_handlebars.rb
@@ -21,7 +21,7 @@ module HandlebarsAssets
       template_namespace = HandlebarsAssets::Config.template_namespace
 
       if HandlebarsAssets::Config.for_ember
-        source_one_line = source.gsub(/\n/,"").gsub(/\r/,"")
+        source_one_line = source.gsub(/([^\n])\n([^\n])/, '\1 \2').chomp
         <<-TEMPLATE
           this.#{template_namespace}[#{template_path.name}] = Ember.Handlebars.compile("#{source_one_line}");
         TEMPLATE
